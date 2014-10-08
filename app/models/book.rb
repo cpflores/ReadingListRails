b8ad4ee3ce65c204258dcbae474afa4937450b03
Chadwick Flores <chadwickflores@gmail.com>
2014-10-07 23:13:29 -0500

    add name to genre filter url

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,7 +5,9 @@ class Book < ActiveRecord::Base
   scope :finished, ->{ where.not(finished_on: nil) }
   scope :recent, ->{ where('finished_on > ?', 2.days.ago) }
   scope :search, ->(keyword){ where('keywords LIKE ?', "%#{keyword.downcase}%") if keyword.present? }
-  scope :filter, ->(genre_id){ joins(:book_genres).where(book_genres: { genre_id: genre_id }) }
+  scope :filter, ->(name){ 
+    joins(:genres).where('genres.name = ?', name) if name.present? 
+  }
 
   before_save :set_keywords
 
